6b7263b0e109f1a0cc014d06e180536c6c4d3455
Scott Malin <mal18008@byui.edu>
2021-01-16 20:02:21 -0700

    Team assignment, personal notes, practice problems.

---
+++ week02/practice/prac02.js
@@ -0,0 +1,24 @@
+// ta02.js
+function sendAlert() {
+   alert("Clicked!")
+}
+function updateDiv() {
+   var textValue = document.getElementById("user_text").value;
+   var section1 = document.getElementById("div1");
+   var color = textValue;
+   section1.style.backgroundColor = color;
+}
+function hideDiv() {
+   $(document).ready(function () {
+      $("#hide").click(function () {
+         $("#div3").hide(1000);
+      });
+      $("#show").click(function () {
+         $("#div3").show(1000);
+      });
+   });
+}
+function createArray(){
+  var arrayName = document.getElementById('array_name').value;
+  document.getElementById('display_box1')
+}
